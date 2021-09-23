654d3e532b6b23d2ba7c0004ff9454b7604cbb37
Nikita <nik_sikora@mail.com>
2021-09-23 19:19:34 +0300

    Добавьте файлы проекта.

---
+++ Task2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(561, 716);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(568, -4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(561, 716);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1, 718);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(561, 716);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(568, 718);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(561, 716);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Location = new System.Drawing.Point(1135, -4);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(771, 716);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea5);
+            this.chart2.Location = new System.Drawing.Point(1135, 722);
+            this.chart2.Name = "chart2";
+            series5.ChartArea = "ChartArea1";
+            series5.Name = "Series1";
+            this.chart2.Series.Add(series5);
+            this.chart2.Size = new System.Drawing.Size(771, 716);
+            this.chart2.TabIndex = 5;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1918, 1450);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Task1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Task2/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Point
+        {
+            public double r;
+            public double g;
+            public double b;
+        }
+
+        static Point[] generateRandomPoints(int count)
+        {
+            var rand = new Random();
+            Point[] points = new Point[count];
+            for (var i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point();
+                points[i].r = rand.Next(20, 235);
+                points[i].g = rand.Next(20, 235);
+                points[i].b = rand.Next(20, 235);
+            }
+            return points;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string img = "D:\\Image\\" + 5 + ".jpg";
+            Bitmap bmp = new Bitmap(img);
+            pictureBox1.Image = Image.FromFile(img);
+
+            int width = bmp.Width;
+            int height = bmp.Height;
+            int POINTS_COUNT = width * height;
+            int[] alfas = new int[POINTS_COUNT];
+            Point[] points = generateRandomPoints(POINTS_COUNT);
+            var num_pixel = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+                    alfas[num_pixel] = a;
+                    points[num_pixel].r = r;
+                    points[num_pixel].g = g;
+                    points[num_pixel].b = b;
+                    num_pixel++;
+                }
+            }
+
+            Bitmap bmp_1 = new Bitmap(bmp);
+            Bitmap bmp_2 = new Bitmap(bmp);
+            Bitmap bmp_3 = new Bitmap(bmp);
+
+            Dictionary<int, int> d1 = new Dictionary<int, int>();
+            Dictionary<int, int> d2 = new Dictionary<int, int>();
+
+            num_pixel = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int r1 = (int)(points[num_pixel].r * 0.299);
+                    int g1 = (int)(points[num_pixel].g * 0.587);
+                    int b1 = (int)(points[num_pixel].b * 0.114);
+                    int k1 = (r1 + g1 + b1);
+                    if (d1.ContainsKey(k1))
+                        d1[k1]++;
+                    else
+                        d1[k1] = 1;
+
+
+                    int r2 = (int)(points[num_pixel].r * 0.2126);
+                    int g2 = (int)(points[num_pixel].g * 0.7152);
+                    int b2 = (int)(points[num_pixel].b * 0.0722);
+                    int k2 = (r2 + g2 + b2);
+                    if (d2.ContainsKey(k2))
+                        d2[k2]++;
+                    else
+                        d2[k2] = 1;
+
+                    int r3 = Math.Abs(k1 - k2);
+                    int g3 = Math.Abs(k1 - k2);
+                    int b3 = Math.Abs(k1 - k2);
+
+                    bmp_1.SetPixel(x, y, Color.FromArgb(alfas[num_pixel], k1, k1, k1));
+                    bmp_2.SetPixel(x, y, Color.FromArgb(alfas[num_pixel], k2, k2, k2));
+                    bmp_3.SetPixel(x, y, Color.FromArgb(alfas[num_pixel], r3, g3, b3));
+                    num_pixel++;
+                }
+            }
+            pictureBox2.Image = bmp_1;
+            pictureBox3.Image = bmp_2;
+            pictureBox4.Image = bmp_3;
+
+            double x_char1, y_char1;
+            this.chart1.Series[0].Points.Clear();
+            x_char1 = 0;
+
+            foreach (var key in d1)
+            {
+                y_char1 = key.Value;
+                this.chart1.Series[0].Points.AddXY(x_char1, y_char1);
+                x_char1 = key.Key;
+            }
+
+            double x_char2, y_char2;
+            this.chart2.Series[0].Points.Clear();
+            x_char2 = 0;
+
+            foreach (var key in d2)
+            {
+                y_char2 = key.Value;
+                this.chart2.Series[0].Points.AddXY(x_char2, y_char2);
+                x_char2 = key.Key;
+            }
+        }
+    }
+}
